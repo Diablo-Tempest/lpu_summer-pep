397ccda6fa516a52a5a076e65da81f6376fdf77b
Biprosom <biprosommajumder@gmail.com>
2026-07-17 16:44:12 +0530

    day20 codes

--- day20/src/App.jsx
+++ day20/src/App.jsx
@@ -1,75 +1,114 @@
-import { useState} from 'react'
-// import { useEffect} from 'react'
-import { useRef } from 'react';
-/* useRef gives us a mutable box that stays the same between renders.
-  It is useful for:
-  1. accessing DOM elements
-  2. storing values that should not cause re-renders.
-  3. keeps track of something without updating UI
-*/
-
+import { useState } from 'react'
+// import { useEffect } from 'react'
+import { useRef } from 'react'
 import './App.css'
-/* Virtual DOM working:
-  1. React creates a virtual DOM tree
-  2. When state or props change, React will make sa new Virtual DOM.
-  3. It compares the old and new trees using a process called reconcillation
-  4. It finds only the parts that changed
-  5. Then it updates only those parts in the real DOM
-*/
-/* useEffect: It is a hook that is used to run side effects in our react app. It let's us run code after react renders a component.
-It is helpful when:
-1. calling an API
-2. Setup a timer.
-3. Adding event listeners
-4. sync with browser features like localStorage, or document.title.
-*/
+
 function App() {
-  // const inputRef = useRef(null);
-  const imgRef = useRef();
-  const imgChange = ()=>{
-    if (imgRef.current.src == 'http://localhost:5173/favicon.svg'){
-      imgRef.current.src = '/react.svg';
-    }
-    else{
-      imgRef.current.src = 'http://localhost:5173/favicon.svg';
-    }
-  }
-  // const focusInput = ()=>{
-    // inputRef.current.focus();
-    // console.log(inputRef.current.value);
-    
-  // }
   const [count, setCount] = useState(0)
+  /*
+    useRef gives us a mutable box that stays the same between renders
+    It is useful for:
+    1. Accessing the DOM
+    2. Storing values that should not cause re-render.
+    3. keep track of something without updating the UI.
+  */
+  const inputRef = useRef(null);
+  const focusInput = ()=>{
+    console.log(inputRef.current.value);
+    
+  }
+  /* useEffect let's us run code after React renders a component. It is responsible for creating side-effects in our app.
+  Side-effects:
+  1. Calling an API
+  2. setting timer
+  3. adding event listeners
+  sync with browser features like localStorage or document.title
+  
+  */
+
   // useEffect(()=>{
-    //code runs after render
   //   document.title = `Count: ${count}`;
   // }, [count])
   // let num = 0;
   // useEffect(()=>{
-  //   let timer = setInterval(()=>{console.log(`Count: ${num++}`);
-  //   }, 2000);
-    //cleanup: if the effcct creates something that should be removed later, return a cleanup function.
-    // return ()=>{
-    //   clearInterval(timer)
-    // }
-  // }, [count])
-  // [] means it will run only one after the first render.
-  // [dependencies] it will run at first after the first render and then only run whenever the dependency is changing.
-  // no dependency array: It will run at every render.
+
+  //   //cleanup function: If our effect creates something that should be removed later, return a cleanup function.
+  //   const timer = setInterval(() => {
+  //     console.log(`Number: ${num++}`);
+  //   }, 1000);
+  //   return ()=>{ clearInterval(timer);
+  //   }
+  // },)
+  /*
+  Dependency Array:
+  1. [] - run once after the first render
+  2. [dependency] - run when the dependency value changes
+  3. No dependency array: run after every render
+  */
+/*
+  Virtual DOM steps:
+  1. React creates a Virtual DOM
+  2. When states or props change, React makes a new Virtual DOM.
+  3. It compares the old and new tree using a process called reconcillation.
+  4. Then it updates only those parts in the real DOM.
+*/
+/*
+Reconcillation: It is the entire process React uses to update the UI.
+Diffing: Comparing old and new virtual DOMs. It is one step inside reconcillation where React compares two virtual DOMS.
+
+function Counter(){
+  const [count, setCount] = useState(0);
+  return{
+    <>
+      <div>{count}</div>
+
+    </div>
+    <button type="button" className="counter" onClick={() => setCount((count) => count + 1)}>Count</button>
+  }
+}
+
+1. State will change
+2. Create a new Virtual DOM
+old virtusl DOM:
+  <div>
+    <h1>0</h1>
+    <button>Increment</button>
+  </div>
+  New Virtual DOM
+    <h1>1</h1>
+    <button>Increment</button>
+  </div>
+3. Diffing
+<div> - same
+<button> - same
+<h1> text changes from 0 to 1
+This comparision is called diffing
+
+4. Updating The Real DOM
+<h1>1</h1>
+This complete reconcillation
+
+  Visual Flow:
+
+  State Changes
+      |
+  Create new Virtusl DOM
+      |
+  Driffing (compares old vs new)
+      |
+  Update Real DOM
+      |
+  Reconcillation Complete
+
+*/
+
   return (
     <>
-    {console.log("App is re-rendered")}
-    <img ref={imgRef} src="/favicon.svg" alt="" height={300} />
-    <button onClick={imgChange}>Change</button>
-      {/* <input ref={inputRef} type="text" />
-      <button onClick={focusInput}>Focus</button> */}
-         <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
+      {console.log("App is re-rendered")
+      }
+        <button type="button" className="counter" onClick={() => setCount((count) => count + 1)}>Count is {count}</button>
+        <input ref={inputRef} type="text" />
+        <button onClick={focusInput}>Click</button>
     </>
   )
 }
